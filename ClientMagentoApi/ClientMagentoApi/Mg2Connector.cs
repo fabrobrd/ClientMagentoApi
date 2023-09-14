cae8d82c831565deeff14737b4934c51057b1069
fabrobrd <74380803+fabrobrd@users.noreply.github.com>
2023-09-14 09:45:09 -0300

    Initial

---
+++ ClientMagentoApi/ClientMagentoApi/Mg2Connector.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ClientMagentoApi
+{
+    public class Mg2Connector
+    {
+        private RestClient _client { get; set; }
+        private Uri _magentoUrl { get; set; }
+        private string _token { get; set; }
+
+        public Mg2Connector(string magentoUrl)
+        {
+            _client = new RestClient(magentoUrl);
+        }
+        public Mg2Connector(string magentoUrl, string token)
+        {
+            _client = new RestClient(magentoUrl);
+        }
+
+        public string GetAdminToken(string userName, string passWord)
+        {
+            var request = CreateRequest("/rest/V1/integration/admin/token", Method.Post);
+            var user = new Credentials();
+            user.userName = userName;
+            user.password = passWord;
+            string json = JsonConvert.SerializeObject(user, Newtonsoft.Json.Formatting.Indented);
+
+            request.AddParameter("application/json", json, ParameterType.RequestBody);
+
+            var response = _client.Execute(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return response.Content;
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+        private RestRequest CreateRequest(string endPoint, Method method)
+        {
+            var request = new RestRequest(endPoint, method);
+            request.RequestFormat = DataFormat.Json;
+            return request;
+        }
+
+    }
+}
